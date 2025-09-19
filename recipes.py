a304c2f1f2de9c80bd933b4520d69e9b7ea6b088
meriem <meriem.bachi@etu.univ-amu.fr>
2025-09-19 14:46:41 +0200

    intégration de l'API recette

---
+++ recipes.py
@@ -0,0 +1,23 @@
+RECIPES = {
+    "spaghetti bolognese": {
+        "ingredients": ["spaghetti", "tomates", "viande hachée", "oignon", "ail"],
+        "actions": [
+            "éplucher l'oignon",
+            "émincer l'oignon",
+            "cuire la viande hachée",
+            "ajouter les tomates",
+            "mélanger la sauce",
+            "faire cuire les spaghetti",
+            "assembler spaghetti + sauce"
+        ]
+    },
+    "salade": {
+        "ingredients": ["laitue", "tomates", "concombre"],
+        "actions": [
+            "laver la laitue",
+            "couper les tomates",
+            "couper le concombre",
+            "mélanger tous les ingrédients"
+        ]
+    }
+}
